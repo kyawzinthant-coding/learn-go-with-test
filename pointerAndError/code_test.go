546f02662e33c8e01853a5e7b164490f3224f3cb
kyawzinthant-coding <kyawthantver2@gmail.com>
2026-05-09 00:34:05 +0630

    chp-6 pointer

---
+++ pointerAndError/code_test.go
@@ -0,0 +1,31 @@
+package pointerAndError
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, w Wallet, want Bitcoin) {
+		t.Helper()
+		got := w.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s ", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+}
